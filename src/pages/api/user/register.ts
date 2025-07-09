9cd2979f3a097d7f5e0efd03e63f68051a2ae802
Denni <dennipes1@gmail.com>
2025-07-09 20:37:24 +0700

    fix: register

--- src/pages/api/user/register.ts
+++ src/pages/api/user/register.ts
@@ -5,21 +5,33 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "POST") {
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      status: false,
+      message: "Method Not Allowed",
+    });
+  }
+
+  try {
     await signUp(req.body, (status: boolean) => {
       if (status) {
-        return res
-          .status(200)
-          .json({ status: true, statusCode: 200, message: "success" });
+        return res.status(200).json({
+          status: true,
+          message: "Success",
+        });
       } else {
-        return res
-          .status(400)
-          .json({ status: false, statusCode: 400, message: "failed" });
+        return res.status(409).json({
+          status: false,
+          message: "Email already registered",
+        });
       }
     });
-  } else {
-    res
-      .status(405)
-      .json({ status: true, statusCode: 405, message: "Method not allowed" });
+  } catch (err: any) {
+    console.error("🔥 Error di handler /api/user/register:", err);
+    return res.status(500).json({
+      status: false,
+      message: "Internal Server Error",
+      error: err.message || err,
+    });
   }
 }
