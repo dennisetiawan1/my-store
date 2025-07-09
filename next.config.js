e035807b7fcabad0bd4e27c9fe52662209b2533f
Denni <dennipes1@gmail.com>
2025-07-09 21:02:12 +0700

    fix: register

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-// next.config.js
-module.exports = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  // Atur lokasi src sebagai root pages
   experimental: {
     appDir: false,
   },
 };
+
+module.exports = nextConfig;
