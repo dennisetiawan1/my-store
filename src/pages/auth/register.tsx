33af46083568ca35a56118d33888ac5e2fb683ab
Denni <dennipes1@gmail.com>
2025-07-09 16:02:11 +0700

    feat: register

---
+++ src/pages/auth/register.tsx
@@ -0,0 +1,10 @@
+import RegisterView from "@/components/views/auth/Register"
+
+const RegisterPage = () => {
+    return(
+        <>
+            <RegisterView></RegisterView>
+        </>
+    )
+}
+export default RegisterPage;
